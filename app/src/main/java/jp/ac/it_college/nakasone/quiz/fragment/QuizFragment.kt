29a758040d4fcc0662c1fd6c8978674aa0f9e558
nakasone <nakasone@std.it-college.ac.jp>
2021-08-24 16:15:16 +0900

    カウントダウンは出来たはず。

--- app/src/main/java/jp/ac/it_college/nakasone/quiz/fragment/QuizFragment.kt
+++ app/src/main/java/jp/ac/it_college/nakasone/quiz/fragment/QuizFragment.kt
@@ -1,6 +1,7 @@
 package jp.ac.it_college.nakasone.quiz.fragment
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,19 +12,40 @@ import jp.ac.it_college.nakasone.quiz.databinding.FragmentQuizBinding
 import jp.ac.it_college.nakasone.quiz.realm.model.Quiz
 import jp.ac.it_college.nakasone.quiz.realm.model.randomChooseQuiz
 
+const val MAX_COUNT = 10
+
 class QuizFragment : Fragment() {
     private var _binding: FragmentQuizBinding? = null
     private val binding get() = _binding!!
-    private var current: Int = 0
+    private var current: Int = -1
+    private var timer = TimeLeftCountdown()
 
-    lateinit var quizList: List<Quiz>
+    private lateinit var quizList: List<Quiz>
 
-    val onChoiceClick = View.OnClickListener { v ->
+    private val onChoiceClick = View.OnClickListener { v ->
+        setBulkEnabled(false)
+        timer.cancel()
         if (v is Button && v.text == quizList[current].choices[0]) {
-            Snackbar.make(v, "正解の処理", Snackbar.LENGTH_SHORT).show()
+            // 正解の表示
+            next()
         } else {
-            Snackbar.make(v, "まちがいの処理", Snackbar.LENGTH_SHORT).show()
+            // 間違いの表示
+            next()
         }
+    }
+
+    inner class TimeLeftCountdown : CountDownTimer(10000, 50) {
+        override fun onTick(millisUntilFinished: Long) {
+            binding.timeLeftBar.progress = millisUntilFinished.toInt() / 10
+        }
+
+        override fun onFinish() {
+            setBulkEnabled(false)
+            binding.timeLeftBar.progress = 0
+            // タイムアップの表示
+            next()
+        }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +70,7 @@ class QuizFragment : Fragment() {
         binding.choiceC.setOnClickListener(onChoiceClick)
         binding.choiceD.setOnClickListener(onChoiceClick)
 
-        setQuiz(0)
+        next()
     }
 
     override fun onDestroyView() {
@@ -56,7 +78,23 @@ class QuizFragment : Fragment() {
         _binding = null
     }
 
-    fun setQuiz(position: Int) {
+    private fun next() {
+        if (++current < MAX_COUNT) {
+            timer.cancel()
+            setQuiz(current)
+            timer.start()
+            setBulkEnabled(true)
+        }
+    }
+
+    private fun setBulkEnabled(flag: Boolean) {
+        binding.choiceA.isEnabled = flag
+        binding.choiceB.isEnabled = flag
+        binding.choiceC.isEnabled = flag
+        binding.choiceD.isEnabled = flag
+    }
+
+    private fun setQuiz(position: Int) {
         binding.quizText.text = quizList[position].question
         if (quizList[position].imageFilename.isNullOrBlank()) {
             binding.quizImage.visibility = View.GONE
@@ -76,4 +114,7 @@ class QuizFragment : Fragment() {
         binding.choiceD.text = randomChoice[3]
     }
 
+    private fun toResult() {
+
+    }
 }
